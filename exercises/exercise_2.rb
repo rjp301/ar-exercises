0cec8b2b10a7e00cb6e331b66989720b90f66f35
rjp301 <rileypaul96@gmail.com>
2022-03-07 16:01:48 -0700

    complete exercise 2

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -4,4 +4,10 @@ require_relative './exercise_1'
 puts "Exercise 2"
 puts "----------"
 
+store1 = Store.find_by(id: 1)
+store2 = Store.find_by(id: 2)
+
+store1.update(annual_revenue: 1200000)
+
+
 # Your code goes here ...

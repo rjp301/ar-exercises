f3c3bca41c8cac6920a9138410912f97a5859fcc
rjp301 <rileypaul96@gmail.com>
2022-03-07 15:52:29 -0700

    complete exercise 1

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -1,10 +1,19 @@
 require_relative '../setup'
+require 'active_record'
 
 puts "Exercise 1"
 puts "----------"
 
 # Your code goes below here ...
-class Store < ApplicationRecord
+class Store < ActiveRecord::Base
+  # attr_accessor :name, :annual_revenue, :mens_apparel, :womens_apparel
 end
 
-Store.create(location: "Burnaby", annual_revenue: 300000, mens_apparel: true, womens_apparel: true)
+# s1 = Store.new
+# s1.name = "Burnaby"
+
+Store.create(name: "Burnaby", annual_revenue: 300000, mens_apparel: true, womens_apparel: true)
+Store.create(name: "Richmond", annual_revenue: 1260000, mens_apparel: false, womens_apparel: true)
+Store.create(name: "Gastown", annual_revenue: 190000, mens_apparel: true, womens_apparel: false)
+
+puts Store.count

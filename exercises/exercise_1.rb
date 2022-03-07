2bcfab6f2d4ceac86540f5d7399ebadf3f78848d
rjp301 <rileypaul96@gmail.com>
2022-03-07 16:52:23 -0700

    complete exercise 5 and 6

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -5,12 +5,6 @@ puts "Exercise 1"
 puts "----------"
 
 # Your code goes below here ...
-class Store < ActiveRecord::Base
-  # attr_accessor :name, :annual_revenue, :mens_apparel, :womens_apparel
-end
-
-# s1 = Store.new
-# s1.name = "Burnaby"
 
 Store.create(name: "Burnaby", annual_revenue: 300000, mens_apparel: true, womens_apparel: true)
 Store.create(name: "Richmond", annual_revenue: 1260000, mens_apparel: false, womens_apparel: true)
--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,6 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+@store1.employees.create(first_name: "Rley", last_name: "Virani", hourly_rate: 45)
+@store1.employees.create(first_name: "Sydney", last_name: "Virani", hourly_rate: 200)

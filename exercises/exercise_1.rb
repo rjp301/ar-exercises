00b91cfbd2c5f253f25d040de2ab15d4726925a0
rjp301 <rileypaul96@gmail.com>
2022-03-07 11:36:38 -0700

    Setup environment with correct versions

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -4,3 +4,7 @@ puts "Exercise 1"
 puts "----------"
 
 # Your code goes below here ...
+class Store < ApplicationRecord
+end
+
+Store.create(location: "Burnaby", annual_revenue: 300000, mens_apparel: true, womens_apparel: true)
